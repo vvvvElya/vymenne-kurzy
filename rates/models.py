def481580176e5eb588a36fe3707f01832c5fb15
vvvvElya <elyav8723@gmail.com>
2025-04-05 22:14:21 +0200

    Исправление ошибки Cannot resolve keyword 'currency' into field.

--- rates/models.py
+++ rates/models.py
@@ -71,7 +71,10 @@ class Prediction(models.Model):
     )
     predicted_value = models.DecimalField(max_digits=10, decimal_places=4)
     model_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)  # Добавляем
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)      # Добавляем
 
     class Meta:
         db_table = 'predictions'
+
 
