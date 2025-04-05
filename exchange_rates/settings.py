51c3a15fc1851003ddd40ed98f833ec2e9e6d216
vvvvElya <elyav8723@gmail.com>
2025-04-05 18:29:12 +0200

    Обновление predictions.py

--- exchange_rates/settings.py
+++ exchange_rates/settings.py
@@ -13,7 +13,7 @@ import os
 import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('.env', encoding='utf-8')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
